9defef9f415bc1a7b95a1781235767f9a616b917
Nachiketh Mamidi <nachiketh.mamidi@sjsu.edu>
2022-11-04 12:28:37 -0700

    Updated Index.ejs and started working on cart page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var ejs = require('ejs')
 var bodyParser = require('body-parser')
 var mysql = require('mysql')
+var session = require('express-session')
 
 mysql.createConnection({
     host:"localhost",
@@ -17,21 +18,28 @@ app.set('view engine', 'ejs')
 
 app.listen(8080);
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({secret:"secret"}))
 
 app.get('/', function(req, res){
-
     var con = mysql.createConnection({
         host:"localhost",
         user:"root",
         password:"",
         database:"CS157A_project"
     })
-
     con.query("SELECT * FROM products", (err, result) => {
         res.render('pages/index.ejs', {result:result})
     });
+})
 
-    
+app.post('/add_to_cart', function(req, res){
+    var id = req.body.id;
+    var name = req.body.name;
+    var category = req.body.category;
+    var image = req.body.image;
+    var product = {id:id, name:name, category:category, image:image}
+
+     
 })
 
 console.log("server running on port 8080");
