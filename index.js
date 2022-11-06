7151444d4c4400478bba80ab4311f8bce90f6cc4
Nachiketh Mamidi <nachiketh.mamidi@sjsu.edu>
2022-11-06 15:11:50 -0800

    create user, login, sign up works, fixing cart

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var express = require('express')
 var ejs = require('ejs')
 var bodyParser = require('body-parser')
+const encoder = bodyParser.urlencoded();
 var mysql = require('mysql')
-var session = require('express-session')
+var session = require('express-session');
+//const { Member, Flight, Gate, Baggage } = require("./models");
+const { sequelize, Sequelize } = require("./models/index");
 
-mysql.createConnection({
+const connection = mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"",
     database:"CS157A_project"
+})
+
+connection.connect(function(error){
+    if (error) throw error
+    else console.log('Connection successfull')
 })
 
 var app = express();
@@ -20,14 +28,36 @@ app.listen(8080);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(session({secret:"secret"}))
 
+
+function isProductInCart(cart, id){
+    for (let i = 0; i < cart.length; i ++){
+        if (cart[i].id == id){
+            return true;
+        }
+    } 
+    return false;
+}
+
 app.get('/', function(req, res){
-    var con = mysql.createConnection({
-        host:"localhost",
-        user:"root",
-        password:"",
-        database:"CS157A_project"
+    res.render('pages/login');
+})
+
+// Login page
+app.post('/', encoder, function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    connection.query("select * from customer where username = ? and password = ?",[username, password], function(error, results, fields){
+        if (results.length > 0) {
+            res.redirect('/index')
+        } else {
+            res.redirect('/')
+        }
+        res.end();
     })
-    con.query("SELECT * FROM products", (err, result) => {
+})
+
+app.get('/index', function(req, res){
+    connection.query("SELECT * FROM products", (err, result) => {
         res.render('pages/index.ejs', {result:result})
     });
 })
@@ -39,7 +69,41 @@ app.post('/add_to_cart', function(req, res){
     var image = req.body.image;
     var product = {id:id, name:name, category:category, image:image}
 
+    if(req.session.cart){
+        var cart = req.session.cart;
+
+        if (!isProductInCart(cart, id)){
+            cart.push(product)
+        } 
+    } else{
+        req.session.cart = [product];
+        var cart = req.session.cart;
+    }
      
+    res.redirect('/cart');
 })
+
+app.get('/cart', function(req, res){
+    var cart = req.session.cart;
+    
+    res.render('pages/cart', {cart:cart});
+
+})
+
+
+app.post('/remove_product', function(req, res){
+    var id = req.body.id;
+    var cart = req.session.cart;
+    
+    for(let i = 0; i < cart.length; i ++){
+        if (cart[i].id == id){
+            cart.splice(cart.indexOf(i), i);
+        }
+    }
+    res.redirect('/cart');
+})
+
+
+
 
 console.log("server running on port 8080");
